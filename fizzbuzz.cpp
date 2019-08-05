b022a25335c5f10bd21708ac3ad9b2d0668b946e
Tinghui Song <tinghuisong@Tinghuis-MacBook-Pro.local>
2019-08-05 23:58:15 +0100

    created fizzbuzz file

---
+++ fizzbuzz.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	for (int i = 0; i < 101; ++i){
+		if ( i%4 == 0 && i%6 == 0){
+			cout << "fizzbuzz ";
+		}
+		else if ( i%4 == 0 ){
+			cout << "fizz ";
+		}
+		else if ( i%6 == 0 ){
+			cout << "buzz ";
+		}
+		else{
+			cout << i << " ";
+		}
+	}
+	return 0;
+}
